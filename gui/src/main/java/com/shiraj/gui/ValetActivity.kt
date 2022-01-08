2a619ceefaa07743dd4b8209c977f2699e0cc48e
Shiraj Sayed <shirajsayed13@gmail.com>
2022-01-08 20:51:21 +0530

    Add navigation drawer

--- gui/src/main/java/com/shiraj/gui/ValetActivity.kt
+++ gui/src/main/java/com/shiraj/gui/ValetActivity.kt
@@ -1,13 +1,58 @@
 package com.shiraj.gui
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ValetActivity : AppCompatActivity() {
+    private lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_valet)
+        drawer = findViewById(R.id.drawer)
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    true
+                }
+            }
+            true
+        }
+
+        val drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
